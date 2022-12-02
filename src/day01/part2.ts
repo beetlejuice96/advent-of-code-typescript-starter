d9a5967f36f898caf236d8b28413f2a6c7702279
beetlejuice96 <dilanbernbae043@gmail.com>
2022-12-01 21:28:10 -0300

    add part 2

--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,31 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input: string): number {
-  return 0;
+  const calories = input.split(/\r?\n/);
+  const caloriesPerElf = [];
+  let countBlankLines = 0;
+  let newElf = [];
+  for (let i = 0; i < calories.length; i++) {
+    if (calories[i] === "") {
+      countBlankLines++;
+      newElf = [];
+    } else {
+      newElf.push(calories[i]);
+      caloriesPerElf[countBlankLines] = newElf;
+    }
+  }
+
+  const totalCaloriesPerElf = caloriesPerElf.map((elf) => {
+    const totalCalories = elf.reduce((acc, curr) => {
+      const caloriesNumber = parseInt(curr);
+      return acc + caloriesNumber;
+    }, 0);
+    return totalCalories;
+  });
+
+  const orderCaloriesByDesc = totalCaloriesPerElf.sort((a, b) => b - a);
+  console.log(orderCaloriesByDesc);
+  const sumFirstTree =
+    orderCaloriesByDesc[0] + orderCaloriesByDesc[1] + orderCaloriesByDesc[2];
+  return sumFirstTree;
 }
